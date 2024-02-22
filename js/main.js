770c88195a3f79d7e3c95c4ee71a093945679a7a
CollinHm <collinhermans7@gmail.com>
2024-02-22 12:59:30 +0100

    finishedd the statue and added audio

--- js/main.js
+++ js/main.js
@@ -16,6 +16,13 @@ const sec = 1000;
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 16;
 
+//speech bubbles
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
+const counterSpeech = document.getElementById("counterSpeech");
+const counterAvatarImg = document.getElementById("counterAvatarImg");
+const mcAudio = document.getElementById("mcAudio");
+const counterAudio = document.getElementById("counterAudio");
+
 //Inventory
 const inventoryBox = document.getElementById("inventoryBox")
 const inventoryList = document.getElementById("inventoryList")
@@ -78,6 +85,21 @@ gameWindow.onclick = function (e) {
             sign.style.opacity = 0.5;
             door1.style.opacity = 1;
 
+            break;
+
+        case "statue":
+            showMessage(mainCharacterSpeech, mcAudio, "Wow, a cool statue!");
+            setTimeout(function () { counterAvatarImg.style.opacity = 1 }, 2 * sec);
+            setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "Hello... Person");
+            setTimeout(showMessage, 8 * sec, mainCharacterSpeech, mcAudio, "YOU CAN TALK?!");
+            setTimeout(showMessage, 12 * sec, counterSpeech, counterAudio, "Yes.. wait you can hear me?");
+            setTimeout(showMessage, 16 * sec, mainCharacterSpeech, mcAudio, "Yes i can! this is amazing.");
+            setTimeout(showMessage, 20 * sec, counterSpeech, counterAudio, "This has never happened before..");
+            setTimeout(showMessage, 24 * sec, mainCharacterSpeech, mcAudio, "Could you help me find the key to the left house?");
+            setTimeout(showMessage, 28 * sec, counterSpeech, counterAudio, "Go to the well, there is a key hidden in there.");
+            setTimeout(showMessage, 32 * sec, mainCharacterSpeech, mcAudio, "Thank you!!");
+
+            setTimeout(function () { counterAvatarImg.style.opacity = 0 }, 36 * sec);
             break;
 
         default:
@@ -147,16 +169,33 @@ function updateInventory(inventory, inventoryList) {
 
 }
 
-function showMessage(targetBalloon) {
-    document.getElementById(targetBalloon).style.opacity = "1";
-    setTimeout(hideMessage, 2 * sec, targetBalloon);
+/**
+ * Shows a message in the speech bubble
+ * @param {getElementById} targetBalloon 
+ * @param {getElementById} targetSound 
+ * @param {string} message 
+ */
+function showMessage(targetBalloon, targetSound, message) {
+    targetSound.currentTime = 0;
+    targetSound.play();
+    targetBalloon.style.opacity = "1";
+    targetBalloon.innerText = message;
+    setTimeout(hideMessage, 4 * sec, targetBalloon, targetSound);
 }
+
+/**
+ * Sets the opacity to 0 so you don't see it anymore
+ * @param {getElementById} targetBalloon
+ * @param {getElementById} targetSound
+ */
+function hideMessage(targetBalloon, targetSound) {
+    targetSound.pause();
+    targetBalloon.style.opacity = "0";
+}
+
 //showMessage("mainCharacterSpeech");
 //showMessage("counterSpeech");
-setTimeout(showMessage, 1 * sec, "mainCharacterSpeech");
-setTimeout(showMessage, 2 * sec, "counterSpeech");
+//setTimeout(showMessage, 1 * sec, mainCharacterSpeech, "hey what's up");
+//setTimeout(showMessage, 2 * sec, counterSpeech, "Yo buddy");
 
-function hideMessage(targetBalloon) {
-    document.getElementById(targetBalloon).style.opacity = "0";
-}
-
+showMessage()
